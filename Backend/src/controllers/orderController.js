78678dee35275c0fe2cf58713416cefde815af4a
Thishan Herath <diniduthishan9@gmail.com>
2026-08-06 00:31:53 +0530

    get order by id

--- Backend/src/controllers/orderController.js
+++ Backend/src/controllers/orderController.js
@@ -17,7 +17,7 @@ export const createOrder = async (req, res) => {
 //Get All Orders (Admin)
 export const getOrder = async(req, res) => {
     try{
-        const orders = await Oder.find({
+        const orders = await Order.find({
             isActive: true
         })
         .populate("user", "firstName lastName email")
@@ -26,5 +26,21 @@ export const getOrder = async(req, res) => {
         res.status(200).json(orders);
     } catch (error) {
         res.status(500).json({ message: "Error fetching orders", error: error.message });
+    }
+}
+
+//Get Order By ID
+export const getOrderById = async(req, res) => {
+    try{
+        const order = await Order.findById(req.params.getOrderById)
+        .populate("user", "firstName lastName email")
+        .populate("items.product", "name images price");
+
+        if(!order){
+            return res.status(404).json({ message: "Order not found" });
+        }
+        res.status(200).json(order);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching order", error: error.message });
     }
 }
